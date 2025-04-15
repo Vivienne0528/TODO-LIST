8405716fac89a0682e10834d04f18979c4033cea
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-04-15 11:58:18 +1000

    fetch api and add new tasks

--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Todo from "@/components/todo";
 const TodoList = () => {
   return (
-        <section className="carousel carousel-vertical rounded-box h-96 overflow-y-auto">
-          <section className="flex flex-col  carousel-item h-auto">
-            <Todo />
-          </section>
-        </section>
+    <></>
+        // <section className="carousel carousel-vertical rounded-box h-96 overflow-y-auto">
+        //   <section className="flex flex-col  carousel-item h-auto">
+        //     <Todo />
+        //   </section>
+        // </section>
   );
 };
 
